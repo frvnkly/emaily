49ef349b36f1e4dd8cad863fe07812b3828673bd
Frank Li <li.26.frank@gmail.com>
2018-05-13 22:00:56 -0400

    check for existing user record before creating new one

--- services/passport.js
+++ services/passport.js
@@ -11,6 +11,13 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
   }, (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then(existingUser => {
+        if (existingUser) {
+
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
   })
 );
