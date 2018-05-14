9f9152a01a569e5baf664b83de3cf6b5b9c12a95
Frank Li <li.26.frank@gmail.com>
2018-05-14 00:39:49 -0400

    define passport serializeUser and deserializeUser functions

--- services/passport.js
+++ services/passport.js
@@ -11,14 +11,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
   }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save()
-            .then(user => done(null, user));
-        } 
-      });
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      } 
+    });
   })
 );
