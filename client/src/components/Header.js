fc2ebde2a66eae1d904ca31b207c9c6dca9454c3
Frank Li <li.26.frank@gmail.com>
2018-05-19 17:29:09 -0400

    Header reflects user authentication status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href='/auth/google'>Login with Google</a></li>;
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className='nav-wrapper'>
           <a className='left brand-logo'>Emaily</a>
           <ul className='right'>
-            <li><a>Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +27,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
