6a630a64c0f0c06e16f27ea09569c26c7bc160d1
Frank Li <li.26.frank@gmail.com>
2018-05-20 23:57:54 -0400

    added stripe payment component to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -10,7 +12,12 @@ class Header extends Component {
       case false:
         return <li><a href='/auth/google'>Login with Google</a></li>;
       default:
-        return <li><a href='/api/logout'>Logout</a></li>;
+        return (
+          <React.Fragment>
+            <li><Payments /></li>
+            <li><a href='/api/logout'>Logout</a></li>
+          </React.Fragment>
+        );
     }
   }
 
