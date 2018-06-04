bda5815932d16db6fcd746e19a27f609b293c748
Frank Li <li.26.frank@gmail.com>
2018-06-04 02:01:18 -0400

    configure backend to handle sendgrid webhook and update mongodb to reflect survey results

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const Path = require('path-parser').default;
+const { URL } = require('url');
+const _ = require('lodash');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -7,13 +11,34 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your input!');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log('request received');
-    console.log(req);
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    _.chain(req.body)
+      .map(({ url, email, }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, ...match, };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        }, {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+        }).exec();
+      })
+      .value();
+
     res.send({});
   });
 
